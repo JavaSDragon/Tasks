6e9d94fb89681e06e22f686a0396f784eff0c569
JavaSDragon <33486797+JavaSDragon@users.noreply.github.com>
2018-01-30 10:36:45 +0300

    promise
    promiseTask

--- promiseTask.js
+++ promiseTask.js
@@ -4,15 +4,42 @@ let addReq=(url)=> {
         xhr.open('GET', url, true);
         xhr.onload = function () {
             let json = JSON.parse(this.responseText);
-            //  if(json.getUsersData){
             resolve(json);
-
-            // }
         }
         xhr.send();
     });
 }
 
-addReq('http://www.json-generator.com/api/json/get/cfQCylRjuG')
-.then(()=>{return addReq('http://www.json-generator.com/api/json/get/cfVGucaXPC')})
-.then(item => console.log(item));
+Promise.all([addReq('http://www.json-generator.com/api/json/get/cevhxOsZnS'),
+addReq('http://www.json-generator.com/api/json/get/cguaPsRxAi'),
+addReq('http://www.json-generator.com/api/json/get/cfDZdmxnDm'),
+addReq('http://www.json-generator.com/api/json/get/cfkrfOjrfS'),
+addReq('http://www.json-generator.com/api/json/get/ceQMMKpidK')]).then(result=>console.log(result)); 
+let newArr=[];
+let addArr=(url)=> {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest()
+        xhr.open('GET', url, true);
+        xhr.onload = function () {
+            let json = JSON.parse(this.responseText);
+            newArr.push(json);
+            resolve(newArr);
+        }
+        xhr.send();
+    });
+}
+
+(addArr('http://www.json-generator.com/api/json/get/cevhxOsZnS'))
+  .then(addArr('http://www.json-generator.com/api/json/get/cguaPsRxAi'))
+  .then(addArr('http://www.json-generator.com/api/json/get/cfDZdmxnDm'))
+  .then(addArr('http://www.json-generator.com/api/json/get/cfkrfOjrfS'))
+  .then(addArr('http://www.json-generator.com/api/json/get/ceQMMKpidK'))
+  .then(result=>console.log(result));
+
+
+
+
+    
+
+
+
