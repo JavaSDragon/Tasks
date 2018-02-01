2a2af407cab7a8fd1dc4aefe6d2cd7b3d37ff717
JavaSDragon <33486797+JavaSDragon@users.noreply.github.com>
2018-02-01 17:38:56 +0300

    Update sumTest.js

--- sumTest.js
+++ sumTest.js
@@ -1,37 +1,16 @@
-function sum() {
-    let result = arguments[0];
- 
-    function summa() {
-        if (arguments.length === 0) {
-            return result;
+const sum1 = function (prop) {
+    let result = prop;
+    const summa = prop => {
+        if (prop === undefined) {
+            prop = 0
         }
-        else {
-            result += arguments[0];
-            return summa;
-        }
-    }
+        result += prop;
+        return summa
+    };
     summa.toString = () => result;
- 
     return summa;
-}
-
-console.log(sum(1)(2)(3)()); // 6
-console.log(sum(1)(2)(3)(4)+1); // 11
-console.log(sum(1)(2)(3)(4)(5)+1); // 16
-
-
-const sum1 = function(prop) {
-  let result = prop;
-  const summa = prop => {
-      if(prop===undefined){
-          prop=0
-        }
-        result+=prop;
-        return summa}; 
-  summa.toString = () => result;            
-  return summa;
 };
 
 console.log(sum1(1)(2)(3)()); // 6
-console.log(sum1(1)(2)(3)(4)+1); // 11
-console.log(sum1(1)(2)(3)(4)(5)+1); // 16
+console.log(sum1(1)(2)(3)(4) + 1); // 11
+console.log(sum1(1)(2)(3)(4)(5) + 1); // 16
